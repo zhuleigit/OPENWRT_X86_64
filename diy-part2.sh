d66a31b37b6081e6abf7b0be5a1d500eb8d872c5
zhuleigit <leizhu.three@gmail.com>
2020-06-13 19:41:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/root::0:0:99999:7:::/root:$1$j6bRkrbq$Bi6YQ5AIvvQBSh8RU06JN/:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's|root::0:0:99999:7:::|root:$1$j6bRkrbq$Bi6YQ5AIvvQBSh8RU06JN/:0:0:99999:7:::|g' package/base-files/files/etc/shadow
